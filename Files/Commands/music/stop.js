39b70196af49ac53cdb60d4ba31a261e2d4d2262
NaturalTwitch <awesomejunior2@gmail.com>
2024-10-01 15:55:34 +0000

    renamed folders

---
+++ Files/Commands/music/stop.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'stop',
+    aliases: ['st'],
+    description: 'Stops music playback',
+    utilisation: '{prefix}stop',
+    voiceChannel: true,
+
+    execute(client, message, cmd, args, Discord) {
+        const queue = client.player.nodes.get(message.guild.id);
+
+        if (!queue || !queue.node.isPlaying()) return message.channel.send(`${message.author}, There is no music currently playing!`);
+
+        queue.delete();
+
+        message.channel.send(`Stopping playback and clearing queue`);
+    },
+};
