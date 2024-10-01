648eed2353d8d723ca6a2a20a9a8eb8c7ca457ca
NaturalTwitch <awesomejunior2@gmail.com>
2024-10-01 02:19:37 +0000

    Help command

--- Files/Commands/misc/help.js
+++ Files/Commands/misc/help.js
@@ -35,3 +35,40 @@ module.exports = {
         }
     }
 }
+
+module.exports = {
+    name: 'help',
+    aliases: ['cmd'],
+    description: `Shows Commands`,
+    async execute(client, message, cmd, args, Discord) {
+        const command = client.commands.get(args[0])
+
+        console.log(command)
+
+        const name = command.name;
+        const cmdaliases = command.aliases || null;
+        const description = command.description;
+        const category = command.category;
+
+        if (command) {
+            const cmdShowcase = new Discord.EmbedBuilder()
+                .setColor(`#000000`)
+                .setTitle(name)
+                .addFields({
+                    name: `Aliases`,
+                    value: cmdaliases,
+                },
+                    {
+                        name: `Descripition`,
+                        value: description
+                    },
+                    {
+                        name: `Catergory`,
+                        value: category
+                    }
+                )
+
+            message.channel.send({ embeds: [cmdShowcase] })
+        }
+    }
+}
