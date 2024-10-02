4284000661f0405a0d994cc66e8aa114f1a8fc15
NaturalTwitch <awesomejunior2@gmail.com>
2024-10-02 01:46:11 +0000

    New Files

---
+++ Files/Commands/music/back.js
@@ -0,0 +1,23 @@
+const { description } = require("../reload");
+
+module.exports = {
+    name: 'back',
+    aliases: [],
+    description: "Returns to previously played song.",
+    utilisation: '{prefix}back',
+    howTo: `.back`,
+    voiceChannel: true,
+
+    async execute(client, message, cmd, args, Discord) {
+        const queue = client.player.nodes.get(message.guild.id);
+
+        if (!queue || !queue.node.isPlaying()) return message.channel.send(`${message.author}, There is no music currently playing! ❌`);
+
+        // const previousTracks = queue.history.toArray();
+        // if (previousTracks.length < 2) return message.channel.send(`\${message.author}, There was no music playing before ❌`);
+
+        await queue.history.back();
+
+        message.channel.send(`Previous music started playing... ✅`);
+    },
+};
---
+++ Files/Commands/music/pause.js
@@ -0,0 +1,20 @@
+const { description } = require("../reload");
+
+module.exports = {
+    name: 'pause',
+    aliases: [],
+    description: "pauses current music",
+    howTo: ".pause",
+    utilisation: '{prefix}pause',
+    voiceChannel: true,
+
+    execute(client, message, cmd, args, Discord) {
+        const queue = client.player.nodes.get(message.guild.id);
+
+       if (!queue || !queue.node.isPlaying()) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+
+        const success = queue.node.pause();
+
+        return message.channel.send(success ? `The currently playing music named **${queue.currentTrack.title}** has stopped ✅` : `${message.author}, Something went wrong. ❌`);
+    },
+};
